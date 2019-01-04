a51175f3a98742a2c47ce42592991f72ac12ca82
Vova Borisenko <vovaborisenko@live.com>
2019-01-04 11:33:45 +0300

    pad version DONE

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 ( function($) { 
-    $('.slider__wrap').slick({slidesToShow: 3}); 
-    $('.offices__wrap').slick();
-    setTimeout(() =>$('.offices__wrap').addClass('section'), 2000);
+    $('.slider__wrap').slick( {slidesToShow: 3} ); 
+    $('.offices__wrap').slick( {infinite: false} );
+    setTimeout( () => $('.offices__wrap').addClass('section'), 2000 );
 } )(jQuery)
