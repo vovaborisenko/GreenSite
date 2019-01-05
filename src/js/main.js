505dcc8abe9e26fc78c53e55bec2423df2d50c66
vovaborisenko <vovaborisenko@live.com>
2019-01-05 14:46:29 +0300

    DONE

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-( function($) { 
+( function($) {
     /** утанавливаем слайдеры */
     $('.slider__wrap').slick( {
         slidesToShow: 3,
@@ -16,20 +16,64 @@
               }
             }
         ]
-    } ); 
-    $('.offices__wrap').slick( {infinite: false} );
-    setTimeout( () => $('.offices__wrap').addClass('section'), 2000 );
+    });
+    $('.offices__wrap').slick( {} );
 
     /** открытие мобильного меню */
     let mobile = document.querySelector('.nav__mobile');
-    mobile.addEventListener('click', (e) => {mobile.classList.toggle('open');console.log(e)} );
+
+    mobile.addEventListener('click', () => { 
+        mobile.classList.toggle('open') 
+    } );
+
     document.addEventListener('click', (e) => {
         if (e.target.classList.contains('desktop__item')) {
             e.target.classList.toggle('open');
-        };
+        }
 
         if (e.target.parentNode.classList.contains('desktop__item')) {
             e.target.parentNode.classList.toggle('open');
         }
     });
-} )(jQuery)
+
+    /** ввод номера телефона */
+    let phone = document.querySelectorAll('.input-phone');
+
+    phone.forEach( (el) => {
+        el.addEventListener( 'focus' , e => {
+            if ( !e.target.value ) {
+                e.target.value = '+7 (';
+            }
+        })
+
+        el.addEventListener( 'keyup', e => {
+            console.log(e);
+            let pattern = '+7 (ddd) ddd-dd-dd';
+            pattern = pattern.replace(/\s/g, 's');
+            pattern = pattern.replace(/(.)/g,'\\$1');
+            for (let i = 8; i < pattern.length; i += 2) {
+                const substr = pattern.substr(0, i);
+                console.log(new RegExp(substr));
+                
+            }
+            if ( e.target.value.length < 4 ) {
+                e.target.value = '+7 (';
+            }
+            if (e.which < 47 || e.which > 58) {
+                e.target.value = e.target.value.replace(/[^\d\+\(\)-\s]/g, '');
+            }
+            if (e.target.value.length === 7) {
+                e.target.value += ') ';
+            }
+            if (e.target.value.length === 12 || e.target.value.length === 15) {
+                e.target.value += '-';
+            }
+            if (e.target.value.length > 18) {
+                e.target.value = e.target.value.replace(/(.{18}).+/g, '$1');
+            }
+            if (e.target.value.search(/\s/g)) {
+                
+            }
+        })
+    } )
+} )(jQuery);
